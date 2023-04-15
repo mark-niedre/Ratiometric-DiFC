8f7ff624f9fa8ad66a5e82fb679c03d6086768f4
NiedreLab <42385483+mark-niedre@users.noreply.github.com>
2023-04-15 16:04:22 -0400

    Update microsphere_processing.m

--- Fluorescent Microspheres/microsphere_processing.m
+++ Fluorescent Microspheres/microsphere_processing.m
@@ -15,7 +15,7 @@ proc_flag =  true;
 rel_thresh = [5,5]; % multiplicative factor for the moving threshold:  peak threshold is rel_thresh*estimated standard deviation
 % Parameters for peak matching routine
 coinc_window = 0.03;                              % length (in seconds) of window to search for coincident peaks
-% ^ Commented out Default Values
+
 
 %-----------------------------------------------------------------------------------------------------------------%
 
@@ -57,40 +57,40 @@ end
 
 % Matches coincident peaks b/w two data sets
 % (ensure that data_bs is two columns long, and that peaks is two structs long)
-disp('Matching coincident peaks')
-[coinc_peaks(1), coinc_peaks(2),scores] = matchCoincPeaks(peaks(1), peaks(2), time);
-plotPeaks(data_bs, time, coinc_peaks, thresh_curve, params, 'Direction', 'coinc')
+% disp('Matching coincident peaks')
+% [coinc_peaks(1), coinc_peaks(2),scores] = matchCoincPeaks(peaks(1), peaks(2), time);
+% plotPeaks(data_bs, time, coinc_peaks, thresh_curve, params, 'Direction', 'coinc')
 
 proc_dat.noise = noise;
 proc_dat.peaks = peaks;
 proc_dat.thresh_curve = thresh_curve;
 
-save([fname '_out.mat'], 'time', 'data_bs', 'params', 'in_dat', 'proc_dat', 'coinc_peaks');
+save([fname '_out.mat'], 'time', 'data_bs', 'params', 'in_dat', 'proc_dat');
 
 % Mean Peak Amplitudes:
-mean_pk_amp = [mean(coinc_peaks(1).pks), mean(coinc_peaks(2).pks)];
+ mean_pk_amp = [mean(peaks(1).pks), mean(peaks(2).pks)];
 
 % Signal to noise ratios:
 snr = 20*log10(mean_pk_amp ./ noise);
 
 % Matched mean peak amplitude ratio
-mpkar = mean_pk_amp(1) ./ mean_pk_amp(2);
+% mpkar = mean_pk_amp(1) ./ mean_pk_amp(2);
 
-% Print some data to console
-outputTable = table(mean_pk_amp', snr', [coinc_peaks.count]',...
-    'VariableNames', {'Mean Matched Peak Amplitude', 'Matched Peak SNR', '# Matched Peaks'},...
-    'RowNames', {params(1).name, params(2).name});
-disp(outputTable)
-
-pkwise_amp_ratio = coinc_peaks(1).pks ./ coinc_peaks(2).pks;
-mean_pkwise_amp_ratio = mean(pkwise_amp_ratio);
+% % Print some data to console
+% outputTable = table(mean_pk_amp', snr', [coinc_peaks.count]',...
+%     'VariableNames', {'Mean Matched Peak Amplitude', 'Matched Peak SNR', '# Matched Peaks'},...
+%     'RowNames', {params(1).name, params(2).name});
+% disp(outputTable)
+% 
+% pkwise_amp_ratio = coinc_peaks(1).pks ./ coinc_peaks(2).pks;
+% mean_pkwise_amp_ratio = mean(pkwise_amp_ratio);
 
 mean_background = mean(data);
 
-fprintf('Matched mean peak amplitude ratio: %5.3f\n', mpkar);
-fprintf('Total peaks in channel 1 (680 nm): %.0f\n', peaks(1).count);
-fprintf('Total peaks in channel 2 (580 nm): %.0f\n', peaks(2).count);
-fprintf('Average of peakwise amplitude ratio: %.3f\n', mean_pkwise_amp_ratio);
+% fprintf('Matched mean peak amplitude ratio: %5.3f\n', mpkar);
+% fprintf('Total peaks in channel 1 (680 nm): %.0f\n', peaks(1).count);
+% fprintf('Total peaks in channel 2 (580 nm): %.0f\n', peaks(2).count);
+% fprintf('Average of peakwise amplitude ratio: %.3f\n', mean_pkwise_amp_ratio);
 
 fprintf('\n\n\n--------------------------------------------------------\n')
 try
